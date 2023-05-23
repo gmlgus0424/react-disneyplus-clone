7c562265796aec1a20e753e686805ba8c52b9deb
gmlgus0424 <gmlgus0424@naver.com>
2023-05-23 11:01:36 +0900

    container components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 
 import './App.css';
 import Nav from './components/Nav';
+import styled from 'styled-components'
 
 function App() {
   return (
   <div className="App">
+    <Container>
     <Nav/>
+    </Container>
   </div>
   );
 }
 
 export default App;
 
-//nav 컴포넌트 가져오기
+const Container= styled.main`
+position : relative;
+min-height: calc(100vh -250px);
+overflow-x: hidden;
+display: block;
+top : 72px;
+padding : 0 calc(3.5vw + 5px);
+
+// 그림자 
+&:after{
+  background: url("/images/home-background.png") center center/ cover no-repeat fixed;
+  content: "";
+  position : absolute;
+  inset: 0px;
+  opacity :1;
+  z-index: -1;
+}
+`
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import styled from 'styled-components'
 
+
 const Nav = () => {
+  const [show,setShow]=useState(false);
+  useEffect(()=>{
+      //로직
+      //스크롤- 스크롤이 50보다 클때는 setShow가 실행 안클때는 실행 안됨
+      window.addEventListener('scroll',()=>{
+        if(window.scrollY>50){
+          setShow(true);
+        }else{
+          setShow(false);
+        }
+      })
+    return ()=>{
+      // 사용하지 않을때의 코드 
+      window.removeEventListener('scroll',()=>{});
+    }
+  },[
+    //베열 없으면 컴포넌트 실행 딱 한번ㅁㄴ 실행 / 만약 여기에 있으면 그게 실행됨 
+  ])
   return (
-    <NavWrapper>
+    <NavWrapper show={show}>
       <Logo>
         <img alt="Disney Plus Logo" src="" onClick={()=>(window.location.href="/")}/>
       </Logo>
@@ -14,13 +33,14 @@ const Nav = () => {
 export default Nav
 
 
+// css(styled-component)
 const NavWrapper=styled.nav`
 position: fixed; //스크롤링해도 픽스
 top:0;
 left:0;
 right:0;
 height: 70px;
-background-color:#090b13;
+background-color:${props=>props.show ? "#090b13":"transparent"};
 display : flex;
 justify-content: space-between; 
 align-items: center;
